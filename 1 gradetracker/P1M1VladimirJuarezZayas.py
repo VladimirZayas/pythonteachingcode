41b35dab57e41228c900107506af6113b06ce748
VladimirZayas <vjuarezz@students.kennesaw.edu>
2022-02-13 14:23:52 -0500

    update

---
+++ 1 gradetracker/P1M1VladimirJuarezZayas.py
@@ -0,0 +1,16 @@
+# Create Allergy check code
+
+# [ ] get input for input_test variable
+input_test = input("What categories of food have you eaten in the past 24 hours: ").lower()
+
+# [ ] print "True" message if "dairy" is in the input or False message if not
+print("Dairy: ", "dairy" in input_test)
+
+# [ ] print True message if "nuts" is in the input or False if not
+print("Nuts: ","nuts" in input_test)
+
+# [ ] Challenge: Check if "seafood" is in the input - print message
+print("Seafood: ","seafood" in input_test)
+
+# [ ] Challenge: Check if "chocolate" is in the input - print message
+print("Chocolate: ", "chocolate" in input_test)
