248904939d88805204e3ba8a10734e99729928ae
VladimirZayas <vjuarezz@students.kennesaw.edu>
2022-04-08 17:58:08 -0400

    Update

--- 6 Web Page with Flask/basic page/script1.py
+++ 6 Web Page with Flask/basic page/script1.py
@@ -16,5 +16,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/Vladimir/')
+def Vladimir():
+    return render_template("juarez.html") 
+
 if __name__=="__main__":
     app.run(debug=True)
